ca7cd832a86b05e1c2d95fa1e73fd4aabf17bb00
Alan Christie <alan.christie@matildapeak.com>
2024-12-18 11:46:07 +0000

    ci: More attrempts to fix the CI

--- bdd/features/steps/awx_utils.py
+++ bdd/features/steps/awx_utils.py
@@ -10,23 +10,23 @@ import yaml
 # For stack testing to work we'll need a number of variables.
 # The AWX host (without a protocol, i.e. 'example.com') and
 # a user that can run the templates we'll be using (set in steps.py).
-_AWX_HOSTNAME: Optional[str] = os.environ.get("BEHAVIOUR_AWX_HOSTNAME")
-_AWX_USERNAME: Optional[str] = os.environ.get("BEHAVIOUR_AWX_USERNAME")
-_AWX_PASSWORD: Optional[str] = os.environ.get("BEHAVIOUR_AWX_PASSWORD")
+_CONTROLLER_HOST: Optional[str] = os.environ.get("CONTROLLER_HOST")
+_CONTROLLER_USERNAME: Optional[str] = os.environ.get("CONTROLLER_USERNAME")
+_CONTROLLER_PASSWORD: Optional[str] = os.environ.get("CONTROLLER_PASSWORD")
 
 
 def get_stack_url(name: str) -> str:
     """Returns the AWX username."""
-    if not _AWX_USERNAME:
-        raise ValueError("BEHAVIOUR_AWX_USERNAME is not set")
-    return f"https://fragalysis-{_AWX_USERNAME}-{name}.xchem-dev.diamond.ac.uk"
+    if not _CONTROLLER_USERNAME:
+        raise ValueError("CONTROLLER_USERNAME is not set")
+    return f"https://fragalysis-{_CONTROLLER_USERNAME}-{name}.xchem-dev.diamond.ac.uk"
 
 
 def get_stack_username() -> str:
     """Returns the AWX username."""
-    if not _AWX_USERNAME:
-        raise ValueError("BEHAVIOUR_AWX_USERNAME is not set")
-    return _AWX_USERNAME
+    if not _CONTROLLER_USERNAME:
+        raise ValueError("CONTROLLER_USERNAME is not set")
+    return _CONTROLLER_USERNAME
 
 
 def launch_awx_job_template(template, *, extra_vars) -> None:
@@ -34,12 +34,12 @@ def launch_awx_job_template(template, *, extra_vars) -> None:
     while also providing extra variables via a temporary file.
     """
 
-    if not _AWX_HOSTNAME:
-        raise ValueError("BEHAVIOUR_AWX_HOSTNAME is not set (e.g. example.com)")
-    if not _AWX_USERNAME:
-        raise ValueError("BEHAVIOUR_AWX_USERNAME is not set")
-    if not _AWX_PASSWORD:
-        raise ValueError("BEHAVIOUR_AWX_PASSWORD is not set")
+    if not _CONTROLLER_HOST:
+        raise ValueError("CONTROLLER_HOST is not set (e.g. example.com)")
+    if not _CONTROLLER_USERNAME:
+        raise ValueError("CONTROLLER_USERNAME is not set")
+    if not _CONTROLLER_PASSWORD:
+        raise ValueError("CONTROLLER_PASSWORD is not set")
 
     print(f"Launching AWX JobTemplate '{template}'...")
     print(f"AWX JobTemplate extra_vars={extra_vars}")
@@ -59,12 +59,12 @@ def launch_awx_job_template(template, *, extra_vars) -> None:
 
         cmd += f" --extra_vars @{fp.name}"
 
-    # Use the environment for sensitive AWX API material
-    my_env = os.environ.copy()
-    my_env["CONTROLLER_HOST"] = f"https://{_AWX_HOSTNAME}"
-    my_env["CONTROLLER_USERNAME"] = _AWX_USERNAME
-    my_env["CONTROLLER_PASSWORD"] = _AWX_PASSWORD
+    # We need the environment to be set for sensitive AWX API material: -
+    #
+    # export CONTROLLER_HOST=https://example.com
+    # export CONTROLLER_USERNAME=username
+    # export CONTROLLER_PASSWORD=password
 
-    _ = subprocess.run(cmd, shell=True, check=True, env=my_env, capture_output=True)
+    _ = subprocess.run(cmd, shell=True, check=True, capture_output=True)
 
     print(f"Successfully launched AWX JobTemplate '{template}'...")
